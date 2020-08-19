1b6fbab896f49cc94e7e8ac0c950163212bee0b8
Mason <sqxccdy@icloud.com>
2020-08-19 14:56:30 +0800

    兼容2

--- http_login_start.py
+++ http_login_start.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import base64
 import execjs
 import hashlib
@@ -5,8 +6,118 @@ import json
 from Crypto.Cipher import PKCS1_v1_5
 from Crypto.PublicKey import RSA
 
-from exceptions import ChangeProxyTerminationError
-from http_login_basic import HttpBasic, now_ts_in_millis
+from redis import StrictRedis
+
+import requests
+import logging
+import re
+import time
+
+requests.packages.urllib3.disable_warnings()
+logging.basicConfig()
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+
+class ChangeProxyTerminationError(SystemError):
+    code = 90009
+
+
+def now_ts_in_millis():
+    return int(time.time() * 1000)
+
+
+class MySession(requests.Session):
+    def post(self, url, data=None, json=None, **kwargs):
+        if 'timeout' not in kwargs:
+            kwargs['timeout'] = 5
+        try:
+            return super(MySession, self).post(url, data, json, **kwargs)
+        except (requests.exceptions.ReadTimeout, requests.exceptions.Timeout):
+            logger.debug('请求连接超时，重试一次. url: {}'.format(url))
+            return super(MySession, self).post(url, data, json, **kwargs)
+
+
+class HttpBasic(object):
+    Request = requests.Request
+
+    def __init__(self, *args, **kwargs):
+        self._crawler_session = MySession()
+        self._crawler_session.headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:74.0) Gecko/20100101 Firefox/74.0',
+            'Accept-Encoding': 'gzip',
+            'Accept-Language': 'zh-CN,zh;q=0.9',
+            'Accept': 'text/javascript, application/javascript, application/ecmascript, application/x-ecmascript, '
+                      '*/*; q=0.01 '
+        }
+        self._crawler_session.verifyCode = ''
+        self._crawler_session.verify = False
+
+    def _quick_get_request(self, *args, **kwargs):
+        return self._crawler_session.get(*args, **kwargs)
+
+    def _quick_post_request(self, *args, **kwargs):
+        return self._crawler_session.post(*args, **kwargs)
+
+    def get_herders(self, req):
+        referer = req.headers.get('Referer')
+        if referer:
+            origin = re.search(r'(.*?://.*?)/', referer, re.M | re.I)
+            if origin:
+                origin = origin.group()[:-1]
+            else:
+                origin = referer
+            req.headers['Host'] = str(origin.split('//')[1])
+            req.headers['Origin'] = str(origin)
+
+    def call(self, req, timeout=None, proxies=None, allow_redirects=True, stream=None, verify=None, cert=None,
+             json=None):
+        # self.get_herders(req)
+        if 'Cookie' not in req.headers:
+            req.headers['Cookie'] = self.get_cookies()
+        # 'Cookie': ';'.join([f'{cookie.name}={cookie.value}'for cookie in self._crawler_session.cookies])
+        #  处理headers
+        # req.headers['x-forwarded-for'] = '39.130.66.145'
+        # req.headers['Proxy-Client-IP'] = '39.130.66.145'
+        # req.headers['WL-Proxy-Client-IP'] = '39.130.66.145'
+        # req.headers['X-Real-IP'] = '39.130.66.145'
+        # req.headers['Proxy-Connection'] = 'keep-alive'
+
+        prep = self._crawler_session.prepare_request(req)
+        proxies = proxies or {}
+        settings = self._crawler_session.merge_environment_settings(
+            prep.url, proxies, stream, verify, cert
+        )
+
+        # Send the request.
+        send_kwargs = {
+            'timeout': timeout,
+            'allow_redirects': allow_redirects,
+        }
+        send_kwargs.update(settings)
+        resp = self._crawler_session.send(prep, **send_kwargs)
+        return resp
+
+    def get_cookies(self):
+        return ';'.join(['{}={}'.format(cookie.name, cookie.value) for cookie in self._crawler_session.cookies])
+
+    def easy_get(self, url, **headers):
+        return self.call(self.Request(method='GET', url=url, headers=headers))
+
+    def warning(self, msg, **kwargs):
+        self.log(logging.WARNING, msg, **kwargs)
+
+    def info(self, msg, **kwargs):
+        self.log(logging.INFO, msg, **kwargs)
+
+    def error(self, msg, **kwargs):
+        self.log(logging.ERROR, msg, **kwargs)
+
+    def debug(self, msg, **kwargs):
+        self.log(logging.DEBUG, msg, **kwargs)
+
+    def log(self, method, msg, **kwargs):
+        logger.log(method, msg)
 
 
 class HttpLoginCMCC(HttpBasic):
@@ -22,7 +133,7 @@ class HttpLoginCMCC(HttpBasic):
     _LOGIN_CHECKOUT_LOGIN = "http://www1.10086.cn/web-Center/authCenter/checkUserLogin.do"
 
     def __init__(self, mobile, *args, **kwargs):
-        super().__init__(mobile, *args, **kwargs)
+        super(HttpLoginCMCC, self).__init__(mobile, *args, **kwargs)
         self.mobile = mobile
         self.veri_time = 0
         self.login_time = 0
@@ -98,7 +209,7 @@ class HttpLoginCMCC(HttpBasic):
             'Host': 'www1.10086.cn',
             'Accept-Encoding': 'gzip, deflate',
             'Accept-Language': 'zh-CN,zh;q=0.9',
-            'Cookie': ';'.join([f'{cookie.name}={cookie.value}' for cookie in self._crawler_session.cookies])
+            'Cookie': self.get_cookies()
         }, data={'requestJson': json.dumps(json_data).replace(' ', '')}), allow_redirects=True)
 
     def need_verify_code(self, mobile, **kwargs):
@@ -109,11 +220,22 @@ class HttpLoginCMCC(HttpBasic):
         # 判断是否以前已经登陆过。则获取verifyCode，检查是否需要发送短信
         self._crawler_session.verifyCode = ''
         self.easy_get(self._INIT_HOME_URL, Referer=self._INIT_HOME_URL)
-        # 设置Cookies: sendflag
         self.easy_get(self._INIT_LOAD_SEND_FLAG.format(now_ts_in_millis()), Referer=self._INIT_HOME_URL)
+        resp = None
+        if self._crawler_session.verifyCode:
+            cur_headers = {
+                'X-Requested-With': 'XMLHttpRequest',
+                'Referer': self._INIT_HOME_URL,
+                'Cookie': 'verifyCode=' + self._crawler_session.verifyCode
+            }
+            resp = self.call(
+                self.Request(method='GET', url=self._INIT_LOGIN_URL.format(self.mobile, now_ts_in_millis()),
+                             headers=cur_headers))
         req = self.Request(method='POST', url=self._INIT_CHK_NUMBER_ACTION, data={"userName": mobile},
                            headers={'Referer': self._INIT_HOME_URL})
         self.call(req)
+        if resp and '{"needVerifyCode":"0"}' == resp.text:
+            return False
         return True
 
     def send_first_code(self, mobile, **kwargs):
@@ -298,8 +420,37 @@ class HttpLoginCMCC(HttpBasic):
     -----END PUBLIC KEY-----'''
         key = RSA.import_key(rsa_publickey)
         passwd = PKCS1_v1_5.new(key)
-        text = base64.b64encode(passwd.encrypt(bytes(k, encoding='utf-8')))
+        text = base64.b64encode(passwd.encrypt(bytes(k)))
         return text
 
 
+HTTP_CMCC = {
 
+}
+rd = StrictRedis()
+
+
+def http_cmcc_send_code(mobile):
+    HTTP_CMCC[mobile] = HttpLoginCMCC(mobile)
+    HTTP_CMCC[mobile].need_verify_code(mobile)
+    # HTTP_CMCC[mobile].send_first_code(mobile)
+    return mobile
+
+
+def http_cmcc_go_login(mobile, sms_code):
+    flag = HTTP_CMCC[mobile].go_login(sms_code=sms_code)
+    if flag:
+        rd.setex('cmcc_cookie_{}'.format(mobile), 7200, HTTP_CMCC[mobile].get_cookies())
+        HTTP_CMCC[mobile].debug(u'设置cookie成功')
+    del HTTP_CMCC[mobile]
+    return flag
+
+
+if __name__ == '__main__':
+    _ = '18258231737'
+    http_cmcc_send_code(_)
+    code = input('sms_code: ')
+    if http_cmcc_go_login(_, code):
+        print('success')
+    else:
+        print('error')
